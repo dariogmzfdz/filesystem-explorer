e60f75187b1b529d86a149d560dc5a512afbd385
dariogmzfdz <dariogomezfdz@gmail.com>
2022-05-24 15:33:35 +0200

    feat(root): added root directory

--- php-files/createDir.php
+++ php-files/createDir.php
@@ -11,8 +11,8 @@ $my_dir .= $_POST["file_name"];
 if (!is_dir($my_dir)) {
   mkdir($my_dir);
   echo "Se ha creado el directorio $my_dir";
-  header('location:./../index.php' . $url);
+  header('location:./../document_list.php' . $url);
 } else {
   echo "el directorio $my_dir ya existe! No lo vamos a crear de nuevo";
-  header('location:./../index.php' . $url);
+  header('location:./../document_list.php' . $url);
 }
